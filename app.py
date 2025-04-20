028aeead46f5aa00713674d40782522d92266919
Vinay S <spartian6360@gmail.com>
2025-04-20 10:11:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ from datetime import datetime
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///todo.db')
+from dotenv import load_dotenv
+load_dotenv()  
+db_url = os.environ.get('DATABASE_URL', 'sqlite:///todos.db')
+
+if db_url.startswith('postgres://'):
+    db_url = db_url.replace('postgres://', 'postgresql://', 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
